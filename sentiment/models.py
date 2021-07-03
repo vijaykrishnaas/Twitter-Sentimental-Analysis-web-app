2fe80b3b5e17be9b2d307257a29a33bb4bd0b2a8
vijaykrishnaa <vk596466@gmail.com>
2021-07-03 22:05:19 +0530

    db improvements

--- sentiment/models.py
+++ sentiment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SearchEntries(models.Model):
+    user_id = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+    search_query = models.CharField(max_length=50)
+    query_date = models.CharField(max_length=20)
+    user_host = models.CharField(max_length=20)
+    user_agent = models.CharField(max_length=100)
