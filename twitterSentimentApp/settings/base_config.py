456bd606b5c370e1bd162b4fef6b4d767ac28713
vijay krishnaa <37221254+vijaykrishnaas@users.noreply.github.com>
2021-07-19 07:47:58 +0530

    adjusting static files

--- twitterSentimentApp/settings/base_config.py
+++ twitterSentimentApp/settings/base_config.py
@@ -131,4 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
