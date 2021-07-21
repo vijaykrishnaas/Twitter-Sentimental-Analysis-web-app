1c20d2baaca51a6d0b372d94b5f357e6f76656f9
vijay krishnaa <37221254+vijaykrishnaas@users.noreply.github.com>
2021-07-21 09:09:49 +0530

    adds white noise configurations

--- twitterSentimentApp/settings/base_config.py
+++ twitterSentimentApp/settings/base_config.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
