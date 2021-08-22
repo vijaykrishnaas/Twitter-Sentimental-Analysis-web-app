355ffd5cdbb8622e8876d7d36b64f2e1f4c99ec6
vijaykrishnaas <vijay.1702128@srit.org>
2021-08-22 10:40:37 +0530

    adds allowed host ip

--- twitterSentimentApp/settings/prod_config.py
+++ twitterSentimentApp/settings/prod_config.py
@@ -3,5 +3,5 @@ from .base_config import *
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = False
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '18.116.100.58']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
