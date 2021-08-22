ffbc5f6e7d68d620a1c8304e8d38ec047b4283af
vijaykrishnaas <vijay.1702128@srit.org>
2021-08-22 21:12:05 +0530

    update allowed host

--- twitterSentimentApp/settings/prod_config.py
+++ twitterSentimentApp/settings/prod_config.py
@@ -3,5 +3,5 @@ from .base_config import *
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '18.116.100.58']
+ALLOWED_HOSTS = ['localhost', '18.116.100.58', '18.116.100.58:8001', 'analysis.twittersentiment.tech']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
