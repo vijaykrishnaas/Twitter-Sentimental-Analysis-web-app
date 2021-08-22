5a98cf5e06b2c7cc8bced8f1a256b24c0260601b
vijaykrishnaas <vijay.1702128@srit.org>
2021-08-22 10:38:34 +0530

    adjust import precedence

--- twitterSentimentApp/asgi.py
+++ twitterSentimentApp/asgi.py
@@ -13,10 +13,11 @@ import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import sentiment.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitterSentimentApp.settings')
 django.setup()
+
+import sentiment.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
